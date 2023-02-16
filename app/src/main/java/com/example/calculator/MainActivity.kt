e447b215f50ab2b070f326ff4b5a07854b3658d4
Nikshit Chauhan <nikshit2012@gmail.com>
2023-02-16 12:22:37 +0530

    Basic Calc complete added operator functions

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -9,6 +9,8 @@ import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     private var tvInput: TextView? = null
+    var lastNumeric : Boolean = false
+    var lastDot : Boolean = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -16,5 +18,90 @@ class MainActivity : AppCompatActivity() {
     }
     fun onDigit(view : View){
         tvInput?.append((view as Button).text)
+        lastNumeric = true
+        lastDot = false
+
+    }
+    fun onClear(view: View){
+        tvInput?.text = ""
+    }
+    fun onDecimal(view: View){
+        if (lastNumeric && !lastDot){
+            tvInput?.append(".")
+            lastNumeric = false
+            lastDot = true
+        }
+    }
+    fun onOperator(view: View){
+        tvInput?.text?.let {
+            if(lastNumeric && !isOperatorAdded(it.toString())){
+                tvInput?.append((view as Button).text)
+                lastNumeric = false
+                lastDot = false
+
+            }
+
+        }
+    }
+    fun onEqual(view: View){
+        if(lastNumeric){
+            var tvValue = tvInput?.text.toString()
+            var prefix = ""
+            try {
+                if(tvValue.startsWith("-")){
+                    prefix = "-"
+                    tvValue = tvValue.substring(1)
+                }
+                if(tvValue.contains("-")){
+                    val splitValue = tvValue.split("-")
+                    var one = splitValue[0]
+                    var two = splitValue[1]
+                    if(prefix.isNotEmpty()){
+                        one = prefix + one
+                    }
+                    tvInput?.text = (one.toDouble() - two.toDouble()).toString()
+
+                }else if(tvValue.contains("+")){
+                    val splitValue = tvValue.split("+")
+                    var one = splitValue[0]
+                    var two = splitValue[1]
+                    if(prefix.isNotEmpty()){
+                        one = prefix + one
+                    }
+                    tvInput?.text = (one.toDouble() + two.toDouble()).toString()
+
+                }else if(tvValue.contains("*")){
+                    val splitValue = tvValue.split("*")
+                    var one = splitValue[0]
+                    var two = splitValue[1]
+                    if(prefix.isNotEmpty()){
+                        one = prefix + one
+                    }
+                    tvInput?.text = (one.toDouble() * two.toDouble()).toString()
+
+                }else if(tvValue.contains("/")){
+                    val splitValue = tvValue.split("/")
+                    var one = splitValue[0]
+                    var two = splitValue[1]
+                    if(prefix.isNotEmpty()){
+                        one = prefix + one
+                    }
+
+                    tvInput?.text = (one.toDouble() / two.toDouble()).toString()
+                }
+            }catch(e : ArithmeticException){
+                e.printStackTrace()
+            }
+        }
+    }
+
+    private fun isOperatorAdded(value : String) : Boolean{
+        return if(value.startsWith("-")){
+            false
+        }else{
+            value.contains("/") || value.contains("*")
+                    || value.contains("+")
+                    || value.contains("-")
+        }
     }
 }
